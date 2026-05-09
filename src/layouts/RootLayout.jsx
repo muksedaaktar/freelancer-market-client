b256c7ee4fd15ed74641b3f02ca890ed7a8794ad
Mukseda Aktar <muksedamukti@gmail.com>
2026-05-09 18:12:24 +0600

    project set up is completed

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+import { Outlet } from "react-router";
+import Navbar from "../components/Navber/Navbar";
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default RootLayout;
